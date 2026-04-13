cec36efd43c392239381e51a974d0e3b98715d08
kshitij-nehete <kshitijnehete@gmail.com>
2026-04-13 19:52:27 +0530

    auth interceptor

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { Inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = Inject(AuthService);
+
+  const token = authService.getToken();
+
+  if (req.url.includes('/auth/login') || req.url.includes('/auth/register')) {
+    return next(req);
+  }
+
+  if (token) {
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return next(clonedReq);
+  }
+
+  return next(req);
+};
