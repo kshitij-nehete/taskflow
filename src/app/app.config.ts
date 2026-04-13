2f1ed9e01e8d5402baa5a75e27331c816f7063c2
kshitij-nehete <kshitijnehete@gmail.com>
2026-04-13 20:15:47 +0530

    added interptor details in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideBrowserGlobalErrorListeners(),
     // provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([])),
+    provideHttpClient(withInterceptors([
+      loadingInterceptor,
+      authInterceptor,
+      errorInterceptor
+    ])),
   ],
 };
