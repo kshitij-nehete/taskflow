d22558bad84b14ccee6541630895ab94558dfb0e
kshitij-nehete <kshitijnehete@gmail.com>
2026-04-15 21:24:55 +0530

    login component (template driven)

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LoginRequest } from '../../../models/auth.model';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
+  standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  credentials: LoginRequest = {
+    email: '',
+    password: '',
+  };
 
+  isLoading = false;
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  onSubmit(form: any): void {
+    if (form.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.credentials).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.errorMessage = response.message || 'Login failed';
+          this.isLoading = false;
+        }
+      },
+
+      error: (err) => {
+        this.errorMessage = err.message || 'Login failed. Please try again';
+        this.isLoading = false;
+      },
+    });
+  }
 }
